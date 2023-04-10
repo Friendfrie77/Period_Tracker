88836ab17430de4a4552927591fd41795d9edf48
Friendfrie77 <awfriend77@gmail.com>
2023-04-10 18:55:31 -0400

    adding vercel to cors

--- backend/config/allowOrigins.js
+++ backend/config/allowOrigins.js
@@ -1,7 +1,8 @@
 const allowedOrigins = [
     'http://localhost:3000',
     'http://192.168.1.140:3000',
-    'https://period-tracker-7426c.web.app'
+    'https://period-tracker-7426c.web.app',
+    'https://period-tracker-three.vercel.app'
 ];
 
 module.exports = allowedOrigins;
